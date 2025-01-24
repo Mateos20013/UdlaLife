74c7ecb0a8e509bfb65569ad652ebd2c321da2fb
Mateo Sebastian Sotomayor Benavides <mateo.sotomayor@udla.edu.ec>
2025-01-23 21:31:00 -0500

    Terminado

--- UdlaLife/Views/MainPage.xaml.cs
+++ UdlaLife/Views/MainPage.xaml.cs
@@ -10,18 +10,24 @@ namespace UdlaLife.Views
             InitializeComponent();
         }
 
+        // Lógica cuando el estudiante hace clic en su botón
         private async void OnStudentClicked(object sender, EventArgs e)
         {
+            // Navegar a la página de login de estudiante
             await Navigation.PushAsync(new LoginPage("Estudiante"));
         }
 
+        // Lógica cuando el profesor hace clic en su botón
         private async void OnProfessorClicked(object sender, EventArgs e)
         {
+            // Navegar a la página de login de profesor
             await Navigation.PushAsync(new LoginPage("Profesor"));
         }
 
+        // Lógica para navegar a la página de registro
         private async void OnRegisterClicked(object sender, EventArgs e)
         {
+            // Aquí puedes redirigir a la página de registro
             await Navigation.PushAsync(new RegisterPage());
         }
     }
